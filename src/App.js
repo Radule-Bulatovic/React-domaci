e9dbc8d1e95b9c986050ba4a0ebde2c270e370d3
Radule-Bulatovic <66704768+Radule-Bulatovic@users.noreply.github.com>
2022-01-06 01:22:46 +0100

    Form validation done, fetching data done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
 import { useEffect, useState } from 'react';
+import { NotificationContainer, NotificationManager } from 'react-notifications';
+import 'react-notifications/lib/notifications.css';
+
 
 function App() {
 
@@ -8,6 +11,7 @@ function App() {
     username: null,
     first: null,
     last: null,
+    email: null,
     password: null,
     password_confirmation: null
   })
@@ -16,6 +20,7 @@ function App() {
     username: null,
     first: null,
     last: null,
+    email: null,
     password: null,
     password_confirmation: null
   })
@@ -28,21 +33,67 @@ function App() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    let tempErrors = { ...errors };
 
-    if(!form.username) {
-      console.log("Username is required field!");
-      setErrors(prev => {return {...prev, username: true}})
-    }else if(form.username.length < 6 || form.username.length > 12) {
-      console.log("Username must be between 6 and 12 characters long!");
-      setErrors(prev => {return {...prev, username: true}})
+    let usernameReg = /[a-zA-Z]{6,12}/
+    if (!usernameReg.test(form.username)) {
+      NotificationManager.error('Username must have between 6 and 12 characters!', 'Error!', 5000);
+      tempErrors.username = true;
+    } else {
+      tempErrors.username = false;
     }
-    
-    if(!form.first) console.log("First name is required field!");
-    if(!form.last) console.log("Last name is required field!");
 
+    if (!form.first) {
+      NotificationManager.error('First name is required field!', 'Error!', 5000);
+      tempErrors.first = true;
+    } else {
+      tempErrors.first = false;
+    }
 
+    if (!form.last) {
+      NotificationManager.error('Last name is required field!', 'Error!', 5000);
+      tempErrors.last = true;
+    } else {
+      tempErrors.last = false;
+    }
 
+    let emailReg = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailReg.test(form.email)) {
+      NotificationManager.error('Email must have valid form!', 'Error!', 5000);
+      tempErrors.email = true;
+    } else {
+      tempErrors.email = false;
+    }
 
+    let passwordReg = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[#$@!%&*?])[A-Za-z\d#$@!%&*?]{8,}$/;
+    if (!passwordReg.test(form.password)) {
+      NotificationManager.error('Password is required field, and must have uppercase, lowercase, number and special character!', 'Error!', 5000);
+      tempErrors.password = true;
+    } else {
+      tempErrors.password = false;
+    }
+
+    if (form.password !== form.password_confirmation || !form.password_confirmation) {
+      NotificationManager.error('Password confirmation is required and must match password', 'Error!', 5000);
+      tempErrors.password_confirmation = true;
+    } else {
+      tempErrors.password_confirmation = false;
+    }
+
+    setErrors({ ...tempErrors })
+    if(Object.values(tempErrors).find(e => e === true)) return;
+
+    let url = "https://jsonblob.com/api/928434760578252800";
+    fetch(url, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(form)
+    })
+      .then((response) => response.json())
+      .then((data) => NotificationManager.success('You have successfuly registered!', 'Succes'))
+      .catch((error) => NotificationManager.error('Server error!', 'Error!', 5000))
   }
 
   return (
@@ -52,34 +103,41 @@ function App() {
           <div className="row">
             <div className="input-field col s6 offset-m3">
               <i className="material-icons prefix">account_circle</i>
-              <input onChange={handleChange} id="username" type="text" className={`validate white-text ${errors.username === true ? "invalid" : null}`} />
+              <input onChange={handleChange} id="username" value={form.username} type="text" className={`white-text ${errors.username === true ? "invalid" : "valid"}`} />
               <label htmlFor="username">Username</label>
             </div>
           </div>
           <div className="row">
             <div className="input-field col offset-m3 s3">
               <i className="material-icons prefix">account_circle</i>
-              <input onChange={handleChange} id="first" type="tel" className={`validate white-text ${errors.first === true ? "invalid" : null}`} />
+              <input onChange={handleChange} id="first" value={form.first} type="text" className={`white-text ${errors.first === true ? "invalid" : "valid"}`} />
               <label htmlFor="first">First name</label>
             </div>
 
             <div className="input-field col s3">
               <i className="material-icons prefix">account_circle</i>
-              <input onChange={handleChange} id="last" type="tel" className={`validate white-text ${errors.last === true ? "invalid" : null}`} />
+              <input onChange={handleChange} id="last" value={form.last} type="text" className={`white-text ${errors.last === true ? "invalid" : "valid"}`} />
               <label htmlFor="last">Last name</label>
             </div>
           </div>
           <div className="row">
             <div className="input-field col s6 offset-m3">
+              <i className="material-icons prefix">email</i>
+              <input onChange={handleChange} id="email" value={form.email} type="email" className={`white-text ${errors.email === true ? "invalid" : "valid"}`} />
+              <label htmlFor="email">Email</label>
+            </div>
+          </div>
+          <div className="row">
+            <div className="input-field col s6 offset-m3">
               <i className="material-icons prefix">vpn_key</i>
-              <input onChange={handleChange} id="password" type="tel" className={`validate white-text ${errors.password === true ? "invalid" : null}`} />
+              <input onChange={handleChange} id="password" value={form.password} type="password" className={`white-text ${errors.password === true ? "invalid" : "valid"}`} />
               <label htmlFor="password">Password</label>
             </div>
           </div>
           <div className="row">
             <div className="input-field col s6 offset-m3">
               <i className="material-icons prefix">vpn_keyx</i>
-              <input onChange={handleChange} id="password_confirmation" type="tel" className={`validate white-text ${errors.password_confirmation === true ? "invalid" : null}`} />
+              <input onChange={handleChange} id="password_confirmation" value={form.password_confirmation} type="password" className={`white-text ${errors.password_confirmation === true ? "invalid" : "valid"}`} />
               <label htmlFor="password_confirmation">Confirm password</label>
             </div>
           </div>
@@ -92,6 +150,7 @@ function App() {
           </div>
         </form>
       </div >
+      <NotificationContainer />
     </div >
   );
 }
